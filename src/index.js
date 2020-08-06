1187ac5f7a8babde2e0145f5f082fcd0bd082b67
YoonsooChang <ysc9606@pusan.ac.kr>
2020-08-07 01:12:42 +0900

    Vanilla Redux Test Done(with no components)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,69 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0;
-const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") return count + 1;
-  else if (action.type === "MINUS") return count - 1;
-  else return count;
+const toDos = [];
+const ADD = "ADD_TODO";
+const DELETE = "DELETE_TODO";
+
+const addToDo = (text) => {
+  return { type: ADD, text };
 };
 
-const store = createStore(countModifier);
-store.subscribe(() => (number.innerText = store.getState()));
+const deleteToDo = (id) => {
+  return { type: DELETE, id };
+};
 
-add.addEventListener("click", () => store.dispatch({ type: "ADD" }));
-minus.addEventListener("click", () => store.dispatch({ type: "MINUS" }));
+const reducer = (state = [], action) => {
+  ul.innerText = "";
+  switch (action.type) {
+    case ADD:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE:
+      return state.filter((e) => e.id !== parseInt(action.id));
+    default:
+      return state;
+  }
+};
+
+const paint = () => {
+  const del = (e) => {
+    const id = e.target.parentNode.id;
+    dispatchDeleteToDo(id);
+  };
+
+  const toDos = store.getState();
+  toDos.forEach((e) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", del);
+    li.id = e.id;
+    li.innerText = e.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+const store = createStore(reducer);
+store.subscribe(() => console.log(store.getState()));
+store.subscribe(paint);
+
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (id) => {
+  store.dispatch(deleteToDo(id));
+};
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddToDo(toDo);
+};
+
+form.addEventListener("submit", onSubmit);
